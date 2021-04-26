e97851f6e32f10d17be5c0ed10350a54e24b34fa
Ather9835 <62211463+Ather9835@users.noreply.github.com>
2021-04-26 14:43:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+import Home from './pages/Home'
+import Faculty from './pages/Faculty'
+import Student from './pages/Student'
+
+
+function App()
+{
+  
+  return (
+    <div>
+        <BrowserRouter>
+                <Route exact path="/"><Home /></Route>
+                <Route path="/faculty"><Faculty /></Route>
+                <Route path="/student"><Student /></Route>
+        </BrowserRouter>
+    
+    </div>
+    
+   
+  );
+}
+
+export default App;
